e9f134c6b63d0f2c5b710ae201f9150d5d583bcf
vinayaksingh7 <vinayaksingh2009@gmail.com>
2021-08-13 13:59:50 +0530

    Changes with web driver invoking and using multiple Junit annotations.

---
+++ src/main/java/com/TestBase/TestBase.java
@@ -0,0 +1,50 @@
+package com.TestBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase {
+
+    public static WebDriver driver;
+    public static Properties prop;
+
+    public TestBase() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/src/test/java/"
+                    + "/config/config.properties");
+            prop.load(ip);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void initialization() {
+        String browserName = prop.getProperty("browser");
+
+        if (browserName.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\vinay\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browserName.equals("FF")) {
+            System.setProperty("webdriver.gecko.driver", "C:\\Users\\vinay\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            driver = new FirefoxDriver();
+        }
+
+        driver.get(prop.getProperty("url"));
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        //Wait
+/*      driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);*/
+
+
+    }
+}
