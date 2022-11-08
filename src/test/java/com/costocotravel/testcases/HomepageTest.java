31a68a99e88917a519ad53a1b84b6afa72454dca
vinayaksingh7 <vinayaksingh2009@gmail.com>
2022-11-08 13:10:46 +0530

    Git webhooks testing

--- src/test/java/com/costocotravel/testcases/HomepageTest.java
+++ src/test/java/com/costocotravel/testcases/HomepageTest.java
@@ -27,6 +27,7 @@ public class HomepageTest extends TestBase {
         String title = homePage.validateHomePageTitle();
         Assert.assertEquals("Home | Costco Travel", title);
         System.out.println("Title is -" + title);
+        System.out.println("Testing webHook");
     }
 
     @After
